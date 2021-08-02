d9ed75c2e3c0d874e070f02aef7850a3d50bc10d
tuan <ngovantuan852@gmail.com>
2021-08-02 09:34:54 +0700

    update

--- src/main/java/org.example.vertxtutorial/service/impl/StudentServiceImpl.java
+++ src/main/java/org.example.vertxtutorial/service/impl/StudentServiceImpl.java
@@ -4,14 +4,26 @@ import io.vertx.core.AsyncResult;
 import io.vertx.core.Future;
 import io.vertx.core.Handler;
 import io.vertx.core.Vertx;
+import io.vertx.core.json.Json;
 import io.vertx.core.json.JsonObject;
+import org.example.vertxtutorial.Entity.StudentEntity;
+
+import java.util.HashMap;
 
 public class StudentServiceImpl implements org.example.vertxtutorial.service.StudentService {
     private Vertx Vertx;
     public StudentServiceImpl (Vertx Vertx){
         this.Vertx =Vertx;
+        createExampleData();
     }
-
+    private static HashMap students=new HashMap();
+    public  void createExampleData()
+    {
+        students.put("1",new StudentEntity("1","1","1","1"));
+        students.put("2",new StudentEntity("2","2","1","1"));
+        students.put("3",new StudentEntity("3","3","1","1"));
+        students.put("4",new StudentEntity("4","4","1","1"));
+    }
 
     @Override
     public void unregisterNoti(JsonObject body, Handler<AsyncResult<Void>> resultHandler)  {
@@ -26,17 +38,33 @@ public class StudentServiceImpl implements org.example.vertxtutorial.service.Stu
 
     @Override
     public void getAllStudent(Handler<AsyncResult<JsonObject>> resultHandler) {
+        if(students.isEmpty()){
+            resultHandler.handle(Future.succeededFuture());
+        }else{
+            JsonObject result = new JsonObject(Json.encode(students));
+            resultHandler.handle(Future.succeededFuture(result));
+        }
     }
 
     @Override
     public void getOneStudent(String id, Handler<AsyncResult<JsonObject>> resultHandler) {
+
+            StudentEntity studentFound = (StudentEntity) students.get(id);
+            if(studentFound == null){
+                resultHandler.handle(Future.succeededFuture());
+            }else{
+                JsonObject result = new JsonObject(Json.encode(studentFound));
+                resultHandler.handle(Future.succeededFuture(result));
+            }
     }
 
     @Override
     public void insertOrUpdateStudent(JsonObject student, Handler<AsyncResult<Void>> asyncResultHandler) {
+
     }
 
     @Override
     public void deleteOneStudent(String id, Handler<AsyncResult<Void>> resultHandler) {
+
     }
 }
--- src/main/java/org.example.vertxtutorial/vertical/StudentVerticle.java
+++ src/main/java/org.example.vertxtutorial/vertical/StudentVerticle.java
@@ -1,18 +1,12 @@
 package org.example.vertxtutorial.vertical;
 
 import io.vertx.core.Promise;
-import io.vertx.core.http.HttpServerResponse;
-import io.vertx.core.json.Json;
+
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.RoutingContext;
 import io.vertx.ext.web.handler.BodyHandler;
-import org.example.vertxtutorial.Entity.StudentEntity;
 import org.example.vertxtutorial.service.StudentService;
-
-
-import java.util.HashMap;
-import java.util.Map;
 
 public class StudentVerticle extends BaseRestVerticle {
     private static final String SERVICE_NAME = "tuangh-rest-api";
@@ -21,17 +15,10 @@ public class StudentVerticle extends BaseRestVerticle {
     public StudentVerticle(StudentService studentService){
         this.studentService=studentService;
     }
-    private static HashMap students=new HashMap();
-    public  void createExampleData()
-    {
-        students.put("1",new StudentEntity("1","1","1","1"));
-        students.put("2",new StudentEntity("2","2","1","1"));
-        students.put("3",new StudentEntity("3","3","1","1"));
-        students.put("4",new StudentEntity("4","4","1","1"));
-    }
+
     @Override
     public void start(Promise<Void> startPromise) throws Exception {
-        createExampleData();
+
         final Router router=Router.router(vertx);
         router.route().handler(BodyHandler.create());
         router.get("/api/students").handler(this::getAllStudent);
@@ -56,69 +43,81 @@ public class StudentVerticle extends BaseRestVerticle {
     }
 
     private void deleteOneStudent(RoutingContext routingContext) {
-        HttpServerResponse response=routingContext.response();
-        response.putHeader("content-type","application/json;charset=UTF8");
-        Map<String,Object> responseBody = new HashMap<>();
-        try {
-            String id = routingContext.request().getParam("id");
-            StudentEntity oldStudent = (StudentEntity) students.get(id);
-            if(oldStudent != null) {
-                students.remove(oldStudent.getStudentId());
-                responseBody.put("status","success");
-                responseBody.put("message","delete success");
-            }else{
-                responseBody.put("status","fail");
-                responseBody.put("message","student not found");
-                response.setStatusCode(400);
-            }
-            response.end(Json.encodePrettily(responseBody));
-        }
-        catch (Exception ex)
-        {
-            response.end(Json.encodePrettily(ex.getMessage()));
-        }
+//        HttpServerResponse response=routingContext.response();
+//        response.putHeader("content-type","application/json;charset=UTF8");
+//        Map<String,Object> responseBody = new HashMap<>();
+//        try {
+//            String id = routingContext.request().getParam("id");
+//            StudentEntity oldStudent = (StudentEntity) students.get(id);
+//            if(oldStudent != null) {
+//                students.remove(oldStudent.getStudentId());
+//                responseBody.put("status","success");
+//                responseBody.put("message","delete success");
+//            }else{
+//                responseBody.put("status","fail");
+//                responseBody.put("message","student not found");
+//                response.setStatusCode(400);
+//            }
+//            response.end(Json.encodePrettily(responseBody));
+//        }
+//        catch (Exception ex)
+//        {
+//            response.end(Json.encodePrettily(ex.getMessage()));
+//        }
+        studentService.deleteOneStudent(routingContext.request().getParam("id"),deleteResultHandler(routingContext));
 
     }
 
     private void updateOneStudent(RoutingContext routingContext) {
-        HttpServerResponse response=routingContext.response();
-        response.putHeader("content-type","application/json;charset=UTF8");
-        Map<String,Object> responseBody = new HashMap<>();
-        try {
-            StudentEntity stu=Json.decodeValue(routingContext.getBody(),StudentEntity.class);
-            StudentEntity oldStudent = (StudentEntity) students.get(stu.getStudentId());
-            if(oldStudent != null) {
-                students.replace(oldStudent.getStudentId(),stu);
-                responseBody.put("status","success");
-                responseBody.put("message","update success");
-            }else{
-                responseBody.put("status","fail");
-                responseBody.put("message","student not found");
-                response.setStatusCode(400);
-            }
-            response.end(Json.encodePrettily(responseBody));
-        }
-        catch (Exception ex)
-        {
-            response.end(Json.encodePrettily(ex.getMessage()));
-        }
+//        HttpServerResponse response=routingContext.response();
+//        response.putHeader("content-type","application/json;charset=UTF8");
+//        Map<String,Object> responseBody = new HashMap<>();
+//        try {
+//            StudentEntity stu=Json.decodeValue(routingContext.getBody(),StudentEntity.class);
+//            StudentEntity oldStudent = (StudentEntity) students.get(stu.getStudentId());
+//            if(oldStudent != null) {
+//                students.replace(oldStudent.getStudentId(),stu);
+//                responseBody.put("status","success");
+//                responseBody.put("message","update success");
+//            }else{
+//                responseBody.put("status","fail");
+//                responseBody.put("message","student not found");
+//                response.setStatusCode(400);
+//            }
+//            response.end(Json.encodePrettily(responseBody));
+//        }
+//        catch (Exception ex)
+//        {
+//            response.end(Json.encodePrettily(ex.getMessage()));
+//        }
+        JsonObject student = routingContext.getBodyAsJson();
+        JsonObject responseBody= new JsonObject();
+        responseBody.put("stastus","200");
+        responseBody.put("message","update success");
+        studentService.insertOrUpdateStudent(student,resultVoidHandler(routingContext,responseBody));
     }
 
     private void insertNewStudent(RoutingContext routingContext) {
-        HttpServerResponse response=routingContext.response();
-        response.putHeader("content-type","application/json;charset=UTF8");
-        Map<String,Object> responseBody = new HashMap<>();
-        try {
-            StudentEntity stu=Json.decodeValue(routingContext.getBody(),StudentEntity.class);
-            students.put(stu.getStudentId(),stu);
-            responseBody.put("status","success");
-            responseBody.put("message","insert success");
-            response.end(Json.encodePrettily(responseBody));
-        }
-        catch (Exception ex)
-        {
-            response.end(Json.encodePrettily(ex.getMessage()));
-        }
+//        HttpServerResponse response=routingContext.response();
+//        response.putHeader("content-type","application/json;charset=UTF8");
+//        Map<String,Object> responseBody = new HashMap<>();
+//        try {
+//            StudentEntity stu=Json.decodeValue(routingContext.getBody(),StudentEntity.class);
+//            students.put(stu.getStudentId(),stu);
+//            responseBody.put("status","success");
+//            responseBody.put("message","insert success");
+//            response.end(Json.encodePrettily(responseBody));
+//        }
+//        catch (Exception ex)
+//        {
+//            response.end(Json.encodePrettily(ex.getMessage()));
+//        }
+        JsonObject student = routingContext.getBodyAsJson();
+        JsonObject responseBody= new JsonObject();
+        responseBody.put("stastus","200");
+        responseBody.put("message","insert success");
+        studentService.insertOrUpdateStudent(student,resultVoidHandler(routingContext,responseBody));
+
     }
 
     public void getAllStudent(RoutingContext routingContext){
@@ -148,26 +147,27 @@ public class StudentVerticle extends BaseRestVerticle {
         studentService.getAllStudent(this.resultHandlerNonEmpty(routingContext));
     }
     public void getOneStudent(RoutingContext routingContext){
-        HttpServerResponse response=routingContext.response();
-        response.putHeader("content-type","application/json;charset=UTF-8");
-        String id = routingContext.request().getParam("id");
-        Map<String,Object> responseBody = new HashMap<>();
-        if(id == null || id.isEmpty()){
-            responseBody.put("status","fail");
-            responseBody.put("message","bad request");
-            response.setStatusCode(400);
-        }else{
-            StudentEntity studentFound = (StudentEntity) students.get(id);
-            if(studentFound == null){
-                responseBody.put("status","success");
-                responseBody.put("message","resource not found");
-                response.setStatusCode(404);
-            }else{
-                responseBody.put("status","Success");
-                responseBody.put("data",studentFound);
-            }
-            response.end(Json.encodePrettily(responseBody));
-        }
+//        HttpServerResponse response=routingContext.response();
+//        response.putHeader("content-type","application/json;charset=UTF-8");
+//        String id = routingContext.request().getParam("id");
+//        Map<String,Object> responseBody = new HashMap<>();
+//        if(id == null || id.isEmpty()){
+//            responseBody.put("status","fail");
+//            responseBody.put("message","bad request");
+//            response.setStatusCode(400);
+//        }else{
+//            StudentEntity studentFound = (StudentEntity) students.get(id);
+//            if(studentFound == null){
+//                responseBody.put("status","success");
+//                responseBody.put("message","resource not found");
+//                response.setStatusCode(404);
+//            }else{
+//                responseBody.put("status","Success");
+//                responseBody.put("data",studentFound);
+//            }
+//            response.end(Json.encodePrettily(responseBody));
+//        }
+        studentService.getOneStudent(routingContext.request().getParam("id"),this.resultHandlerNonEmpty(routingContext));
 
     }
     private void unregisterNoti(RoutingContext routingContext)  {
